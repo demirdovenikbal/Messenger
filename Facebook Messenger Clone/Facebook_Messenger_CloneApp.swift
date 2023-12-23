7381d2190bba1a51cfb76ce3bf9740e01dd66db5
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-23 14:11:48 -0800

    Initial Commit

---
+++ Facebook Messenger Clone/Facebook_Messenger_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook_Messenger_CloneApp.swift
+//  Facebook Messenger Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-23.
+//
+
+import SwiftUI
+
+@main
+struct Facebook_Messenger_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
